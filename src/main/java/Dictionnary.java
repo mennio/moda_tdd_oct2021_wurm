04c60cecded29f414b52fec7c3355c6f11e43739
mennio <menniozikluv@gmail.com>
2021-10-28 11:40:31 +0200

    tache1-2-3

---
+++ src/main/java/Dictionnary.java
@@ -0,0 +1,11 @@
+public class Dictionnary {
+    String nom;
+
+    public Dictionnary(String nom){
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
---
+++ src/test/java/DictionnaryTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class DictionnaryTest {
+
+    private Dictionnary dictionnaire;
+
+    @Test
+    public void testInutile(){
+        assertFalse(false);
+    }
+
+    @Before
+    public void initConstruct(){
+        dictionnaire = new Dictionnary("testFirst");
+    }
+
+    @Test public void TestDictionaryName(){
+        assertThat(dictionnaire.getNom(),equalTo("testFirst"));
+    }
+}
